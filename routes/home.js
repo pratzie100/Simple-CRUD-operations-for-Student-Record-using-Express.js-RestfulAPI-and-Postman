fa00f8e05df6562fa072e09222ca287f950520b7
Pratyush Kargeti <135763048+pratzie100@users.noreply.github.com>
2024-07-04 12:25:05 +0530

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("Welcome to Vidly!");
+  res.send("Welcome to Express!");
 });
 
 module.exports = router;
